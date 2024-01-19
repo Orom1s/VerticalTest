7b4e2dbde922df900370e7b82c7eb746c91ee865
Orom1s <zenkincw@gmail.com>
2024-01-19 12:51:22 +0300

    commit

--- APRK_Parser.cpp
+++ APRK_Parser.cpp
@@ -10,21 +10,6 @@
 using namespace std::literals;
 
 
-/*
-std::vector<std::string_view> SplitIntoWordsView(std::string_view str) {
-    std::vector<std::string_view> result;
-    str.remove_prefix(std::min(str.size(), str.find_first_not_of(' ')));
-    const int64_t pos_end = str.npos;
-
-    while (!str.empty()) {
-        int64_t space = str.find(' ');
-        result.push_back(space == pos_end ? str.substr(0, str.size()) : str.substr(0, space));
-        str.remove_prefix(std::min(str.size(), str.find_first_not_of(' ', space)));
-    }
-
-    return result;
-}
-*/
 class Parser {
 public:
 	Parser(std::ifstream& aprk) {
